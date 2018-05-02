39a461f0e836ab13c5fd457aea87686ccb6dc08e
MaxLich <lich-max@mail.ru>
2018-05-02 16:10:47 +0300

    make tests for LineGroupsFinder class
    details:
    - create test-class for LineGroupsFinder class
    - some refactoring in the LineGroupsFinder class and in the LineGroupsProcessorTest class

--- src/test/java/ru/maxlich/app/processing/LineGroupsProcessorTest.java
+++ src/test/java/ru/maxlich/app/processing/LineGroupsProcessorTest.java
@@ -10,29 +10,31 @@ import static org.junit.Assert.*;
  * Created by mshulakov on 26.04.2018.
  */
 public class LineGroupsProcessorTest {
-    private static List<List<String>> testGroupList;
-
-    @BeforeClass
-    public static void setUpAll() throws Exception {
-        testGroupList = new ArrayList<>();
-        testGroupList.add(Arrays.asList("u;v;y"));
-        testGroupList.add(Arrays.asList("x;y;z", "y;y;w"));
-        testGroupList.add(Arrays.asList("a;b;c", "a;b;d", "c;b;o"));
-    }
-
     @Test
     public void testCountingLineGroups() throws Exception {
+        List<List<String>> testGroupList = new ArrayList<>();
+        testGroupList.add(Arrays.asList("u;v;y"));
+        testGroupList.add(Arrays.asList("a;b;c", "a;b;d", "c;b;o"));
+        testGroupList.add(Arrays.asList("x;y;z", "y;y;w"));
         LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
-        assertEquals(2, lineGroupsProcessor.sortAndCount().getGroupCount());
+        int expectedGrCount = 2;
+        assertEquals(expectedGrCount, lineGroupsProcessor.sortAndCount().getGroupCount());
     }
 
     @Test
     public void testSortingLineGroups() throws Exception {
+        List<List<String>> testGroupList = new ArrayList<>();
+        testGroupList.add(Arrays.asList("u;v;y"));
+        testGroupList.add(Arrays.asList("x;y;z", "y;y;w"));
+        testGroupList.add(Arrays.asList("a;b;c", "a;b;d", "c;b;o"));
         LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
 
         SortedSet<List<String>> sortedLineGroups = lineGroupsProcessor
                 .sortAndCount()
                 .getSortedLineGroups();
+
+        assertNotNull(sortedLineGroups);
+        assertFalse(sortedLineGroups.isEmpty());
 
         int firstGrSize = sortedLineGroups.first().size();
         int lastGrSize = sortedLineGroups.last().size();
@@ -40,8 +42,70 @@ public class LineGroupsProcessorTest {
         assertTrue(firstGrSize >= lastGrSize);
     }
 
-    @AfterClass
-    public static void tearDownAll() throws Exception {
-        testGroupList = null;
+
+    @Test
+    public void testSortAndCountWithNullLineGroupList() {
+        List<List<String>> testGroupList = null;
+        LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
+        lineGroupsProcessor.sortAndCount();
+
+        int expectedGrCount = 0;
+        assertEquals(expectedGrCount, lineGroupsProcessor.getGroupCount());
+
+        SortedSet<List<String>> sortedLineGroups = lineGroupsProcessor.getSortedLineGroups();
+        assertNotNull(sortedLineGroups);
+        assertTrue(sortedLineGroups.isEmpty());
+    }
+    @Test
+    public void testSortAndCountWithEmptyLineGroupList() {
+        List<List<String>> testGroupList = Collections.emptyList();
+        LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
+        lineGroupsProcessor.sortAndCount();
+
+        int expectedGrCount = 0;
+        assertEquals(expectedGrCount, lineGroupsProcessor.getGroupCount());
+
+        SortedSet<List<String>> sortedLineGroups = lineGroupsProcessor.getSortedLineGroups();
+        assertNotNull(sortedLineGroups);
+        assertTrue(sortedLineGroups.isEmpty());
+    }
+    @Test
+    public void testSortAndCountWithSingleLineGroupList() {
+        List<List<String>> testGroupList = new ArrayList<>();
+        testGroupList.add(Collections.singletonList("u;v;y"));
+        LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
+        lineGroupsProcessor.sortAndCount();
+
+        int expectedGrCount = 0;
+        assertEquals(expectedGrCount, lineGroupsProcessor.getGroupCount());
+
+        SortedSet<List<String>> sortedLineGroups = lineGroupsProcessor.getSortedLineGroups();
+        assertNotNull(sortedLineGroups);
+        int expectedTotalGrCount = 1;
+        assertEquals(expectedTotalGrCount, sortedLineGroups.size());
+
+        List<String> firstGroupFromSortedSet = sortedLineGroups.first();
+        List<String> firstGroupFromSourceGroupList = testGroupList.get(0);
+        assertEquals(firstGroupFromSortedSet,firstGroupFromSourceGroupList);
+    }
+
+    @Test
+    public void testSortAndCountWithLineGroupListWithTwoLinesGroup() {
+        List<List<String>> testGroupList = new ArrayList<>();
+        testGroupList.add(Arrays.asList("x;y;z", "y;y;w"));
+        LineGroupsProcessor lineGroupsProcessor = new LineGroupsProcessor(testGroupList);
+        lineGroupsProcessor.sortAndCount();
+
+        int expectedGrCount = 1;
+        assertEquals(expectedGrCount, lineGroupsProcessor.getGroupCount());
+
+        SortedSet<List<String>> sortedLineGroups = lineGroupsProcessor.getSortedLineGroups();
+        assertNotNull(sortedLineGroups);
+        int expectedTotalGrCount = 1;
+        assertEquals(expectedTotalGrCount, sortedLineGroups.size());
+
+        List<String> firstGroupFromSortedSet = sortedLineGroups.first();
+        List<String> firstGroupFromSourceGroupList = testGroupList.get(0);
+        assertEquals(firstGroupFromSortedSet,firstGroupFromSourceGroupList);
     }
 }
